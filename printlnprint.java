eca37ab2ddfd2f0f18611fd020fb698c5f388016
SAUMYA RANJAN MOHAPATRA <111070899+saumyaranjan2003@users.noreply.github.com>
2023-02-14 20:35:20 +0530

    Add files via upload
    hello

---
+++ printlnprint.java
@@ -0,0 +1,13 @@
+public class printlnprint {
+    public static void main(String [] args) {
+        // Different Between Println & Print
+        // <- Comments (Ignore the text you written after this)
+        //Println - to print in next line
+        System.out.println("Hello Good Eveing");
+        //print - to print in same line (no print in next line )
+        System.out.print("Hello World ");
+        System.out.println("Hello Coders");
+        
+    }
+
+}
