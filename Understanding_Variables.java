24d437d6c07174be33ddfa35f10bd21caa9866ec
SAUMYA RANJAN MOHAPATRA <111070899+saumyaranjan2003@users.noreply.github.com>
2023-02-14 22:01:26 +0530

    Add files via upload
    Understanding_Variables

---
+++ Understanding_Variables.java
@@ -0,0 +1,18 @@
+public class Understanding_Variables {
+    public static void main(String args[]) {
+        int i = 10;
+        System.out.println(i);
+        short j = 20;
+        System.out.println(j);
+        long k = 30;
+        System.out.println(k);
+        float l = 1254446;
+        System.out.println(l);
+        double m = 1.26;
+        System.out.println(m);
+        String n = "abcd";
+        System.out.println(n);    
+        boolean mybool = true;
+        System.out.println(mybool);   
+    }
+}
