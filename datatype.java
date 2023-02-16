99d5fe9278c15c01f8d6758b18e78c3fdac2445c
SAUMYA RANJAN MOHAPATRA <111070899+saumyaranjan2003@users.noreply.github.com>
2023-02-16 23:17:27 +0530

    Add files via upload

---
+++ datatype.java
@@ -0,0 +1,24 @@
+public class datatype{
+    public static void main(String[] args) {
+        char c = 'a';
+        System.out.println('a'+3);
+        
+        int i = c+10;
+        System.out.println(i);
+        
+        i =c;
+        //char abc  = i;
+       // long l = i;
+        //i = l;
+        //double d = l;
+        //i = d;
+        //float f = 1.28;
+
+
+
+
+
+        
+    }
+        
+}
