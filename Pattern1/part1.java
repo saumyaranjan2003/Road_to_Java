a12429386bbac75af67e8ed543bf0f5b28d9fbc9
SAUMYA RANJAN MOHAPATRA <111070899+saumyaranjan2003@users.noreply.github.com>
2023-02-21 20:29:12 +0530

    Update part1.java
    update the file

--- Pattern1/part1.java
+++ Pattern1/part1.java
@@ -1,1 +1,22 @@
+import java.util.Scanner;
+public class part1{
+    public static void main(String[] args) {
+        int i =1;
+        int num;
+        Scanner sc = new Scanner(System.in);
+        num = sc.nextInt();
+        while(i<=num)
+        {
+            int j=1;
+            while(j<=num)
+            {
+                System.out.print("*");
+                j++;
+            }
+            System.out.println();
+            i++;
+        }
+        sc.close();
+    }
+}
 
