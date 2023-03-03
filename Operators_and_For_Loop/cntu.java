e593c4e0c75808e689fd435ae2cda705e0a6ff9b
SAUMYA RANJAN MOHAPATRA <111070899+saumyaranjan2003@users.noreply.github.com>
2023-03-03 22:47:21 +0530

    Add files via upload

---
+++ Operators_and_For_Loop/cntu.java
@@ -0,0 +1,12 @@
+public class cntu {
+    public static void main(String[] args) {
+        int i=1;
+        while(i<=10){
+            if(i==5){
+                continue;
+            }
+            System.out.println(i);
+            i=i+1;
+        }
+    }
+}
---
+++ Operators_and_For_Loop/cntu2.java
@@ -0,0 +1,10 @@
+public class cntu2 {
+    public static void main(String[] args) {
+        for(int k = 1;k<=10;k++){
+            if(k==5){
+                continue;
+            }
+            System.out.println(k);
+        }
+    }
+}
