e97341cc06f2af81083ff3cf69e359af7e89ff10
SAUMYA RANJAN MOHAPATRA <111070899+saumyaranjan2003@users.noreply.github.com>
2023-03-04 21:40:25 +0530

    for loop
    for loop last part

---
+++ Operators_and_For_Loop/precide.java
@@ -0,0 +1,8 @@
+public class precide {
+    public static void main(String[] args) {
+        int i = 10;
+        //int a = ++i*5;
+        boolean a = i+5>5;
+        System.out.println(a);
+    }
+}
