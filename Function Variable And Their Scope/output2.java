b7322fe7e217c6e015894536984ffd57d02a2782
SAUMYA RANJAN MOHAPATRA <111070899+saumyaranjan2003@users.noreply.github.com>
2023-03-05 22:49:58 +0530

    Function Variable And Their Scope
    Today Task

---
+++ Function Variable And Their Scope/output2.java
@@ -0,0 +1,11 @@
+public class output2{
+    public static void func(int a, int b){
+        System.out.println(a + b);
+        }
+    
+        public static void main(String[] args) {
+        int a = 7;
+        func(a, 12);
+        }
+    
+}
---
+++ Function Variable And Their Scope/output4.java
@@ -0,0 +1,10 @@
+public class output4 {
+    public static double add(int a,int b)
+{
+    float c=a+b;
+    return c;
+}
+public static void main (String[] args) {
+    System.out.print(add(10,3));
+}
+}
