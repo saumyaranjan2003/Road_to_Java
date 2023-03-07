1f34326f2a26f5d28bcf93789c5e6de9a17b9873
SAUMYA RANJAN MOHAPATRA <111070899+saumyaranjan2003@users.noreply.github.com>
2023-03-07 20:25:28 +0530

    Function Variable And Their Scope
    TODAY TASK

---
+++ Function Variable And Their Scope/output13.java
@@ -0,0 +1,17 @@
+/*Check Prime */
+public class output13{
+    public static boolean isPrime(int x)
+{
+    for(int i=2;i<x/2;i++)
+    {
+        if(x%i==0)
+        return false;
+    }
+    return true;
+
+}
+public static void main (String[] args) {
+    System.out.print(isPrime(47));
+}
+}
+/*The loop would execute for i=2 to 22. And 47 is not divisible by any of them. */
---
+++ Function Variable And Their Scope/output14.java
@@ -0,0 +1,18 @@
+public class output14 {
+    public static int sum(int a,int b)
+{
+    System.out.print("int sum ");
+    return a+b;
+}
+public static long sum(long a,long b)
+{
+    System.out.print("long sum ");
+    return a+b;
+}
+public static void main (String[] args) {
+        int a=4;
+        int b=5;
+    System.out.print(sum(a,b));
+}
+}
+/*As both the datatypes are int So first function would be called. */
