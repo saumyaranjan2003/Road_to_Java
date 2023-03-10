c9c1a1f0633d79bd0e6bc2e6a9687074217d5baf
SAUMYA RANJAN MOHAPATRA <111070899+saumyaranjan2003@users.noreply.github.com>
2023-03-10 21:19:24 +0530

    Create array.java
    ARRAY

---
+++ Arrays/array.java
@@ -0,0 +1,18 @@
+public class array {
+    public static void main(String[] args) {
+        int arr[] = new int[10];
+    }
+}
+
+/*how to declare
+int -> type of array
+arr -> name 
+[] -> indicates this is array
+new -> allocate memory
+[N]_> SIZE OF THE ARRAY
+*/
+
+/*INDEX START WITH 0 */
+/*EX END IS 10
+ * SO IT WILL BE START FROM 0 AND END IN 9
+ */
