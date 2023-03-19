fe15d5403eb411d6e5d62748a99def1b1fade815
SAUMYA RANJAN MOHAPATRA <111070899+saumyaranjan2003@users.noreply.github.com>
2023-03-19 20:47:35 +0530

    Create sort012.java
    Sort 0 1 2

---
+++ Searching And Sorting/Asg/sort012.java
@@ -0,0 +1,85 @@
+/*Sort 0 1 2
+Send Feedback
+You are given an integer array/list(ARR) of size N. It contains only 0s, 1s and 2s. Write a solution to sort this array/list in a 'single scan'.
+'Single Scan' refers to iterating over the array/list just once or to put it in other words, you will be visiting each element in the array/list just once.
+Note:
+You need to change in the given array/list itself. Hence, no need to return or print anything. 
+Input format :
+The first line contains an Integer 't' which denotes the number of test cases or queries to be run. Then the test cases follow.
+
+First line of each test case or query contains an integer 'N' representing the size of the array/list.
+
+Second line contains 'N' single space separated integers(all 0s, 1s and 2s) representing the elements in the array/list.
+Output Format :
+For each test case, print the sorted array/list elements in a row separated by a single space.
+
+Output for every test case will be printed in a separate line.
+Constraints :
+1 <= t <= 10^2
+0 <= N <= 10^5
+Time Limit: 1 sec
+Sample Input 1:
+1
+7
+0 1 2 0 2 0 1
+Sample Output 1:
+0 0 0 1 1 2 2 
+Sample Input 2:
+2
+5
+2 2 0 1 1
+7
+0 1 2 0 1 2 0
+Sample Output 2:
+0 1 1 2 2 
+0 0 0 1 1 2 2 */
+import java.util.Scanner;
+
+public class sort012 {
+    public static void sort012(int[] arr) {
+        int low = 0, mid = 0, high = arr.length - 1;
+        while (mid <= high) {
+            switch (arr[mid]) {
+                case 0:
+                    // If the current element is 0
+                    // swap it with the element at index low
+                    int temp = arr[low];
+                    arr[low] = arr[mid];
+                    arr[mid] = temp;
+                    low++;
+                    mid++;
+                    break;
+                case 1:
+                    // If the current element is 1
+                    // just move to the next element
+                    mid++;
+                    break;
+                case 2:
+                    // If the current element is 2
+                    // swap it with the element at index high
+                    int temp2 = arr[high];
+                    arr[high] = arr[mid];
+                    arr[mid] = temp2;
+                    high--;
+                    break;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        while (t-- > 0) {
+            int n = scan.nextInt();
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = scan.nextInt();
+            }
+            sort012(arr);
+            for (int i = 0; i < n; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
